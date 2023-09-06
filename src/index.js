0d89f675f560be744bb0c698761e1e4f580a86c3
sethgonzales <sethgonzales157@gmail.com>
2023-09-06 10:01:29 -0700

    comment functions of old code

--- src/index.js
+++ src/index.js
@@ -4,42 +4,42 @@ import './css/styles.css';
 
 // Business Logic
 
-function getWeather(city) {
-  let request = new XMLHttpRequest();
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+function getWeather(city) { //run function to access api and create an instance of the code
+  let request = new XMLHttpRequest(); //new instance of the api requested
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`; //url for calling the api and entering api key
 
-  request.addEventListener("loadend", function() {
-    const response = JSON.parse(this.responseText);
-    if (this.status === 200) {
+  request.addEventListener("loadend", function() { //listener function for once the api has loaded
+    const response = JSON.parse(this.responseText); //api is a JSON like object. parse the object to be in actual JS lang
+    if (this.status === 200) { //code 200 means OK. If okay, then go to function to give results
       printElements(response, city);
-    } else {
+    } else { //if not okay, go to the function to print and error
       printError(this, city);
     }
   });
 
-  request.open("GET", url, true);
-  request.send();
+  request.open("GET", url, true); //"open" the api request, telling it we need to GET information from the url, and that this api is async in our application
+  request.send(); //send the request
 }
 
 // UI Logic
 
-function printElements(apiResponse, city) {
+function printElements(apiResponse, city) { //when the api is called correctly, we get this result
   document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%.
-  The temperature in Kelvins is ${apiResponse.main.temp} degrees.`;
+  The temperature in Kelvins is ${apiResponse.main.temp} degrees.`; //object literals like ${city} are used to place parameters and data that is passed through the function.
 }
 
-function handleFormSubmission(event) {
+function handleFormSubmission(event) { //gets the form information to pass through the api, and gather info on the desired choices
   event.preventDefault();
   const city = document.querySelector('#location').value;
   document.querySelector('#location').value = null;
-  getWeather(city);
+  getWeather(city); //once info is gathered, run the function that processes the info through the api
 }
 
-function printError(request, city) {
-  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}:  ${request.status} ${request.statusText}`;
+function printError(request, city) { //instructions for what to do when an error happens
+  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}:  ${request.status} ${request.statusText}`; //status and statusText are properties of the api object. Will give generic info as to what is happening
 }
 
-function checkNumber(number) {
+function checkNumber(number) { //try...catch to see if we have an invalid entry given by the user
   if (isNaN(number) || number < 0) {
     throw "Not a valid number!";
   } else {
