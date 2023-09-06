42136522eaead7c20cb913e93389d84a83931962
sethgonzales <sethgonzales157@gmail.com>
2023-09-06 09:23:02 -0700

    add new fetch code to file

--- src/index.js
+++ src/index.js
@@ -1,35 +1,31 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import WeatherService from './weather-service.js';
 
 // Business Logic
 
 function getWeather(city) {
-  let request = new XMLHttpRequest();
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-
-  request.addEventListener("loadend", function() {
-    const response = JSON.parse(this.responseText);
-    if (this.status === 200) {
-      printElements(response, city);
-    } else {
-      printError(this, response, city);
-    }
-  });
-
-  request.open("GET", url, true);
-  request.send();
+  WeatherService.getWeather(city)
+    .then(function(response) {
+      if (response.main) {
+        printElements(response, city);
+      } else {
+        printError(response, city);
+      }
+    });
 }
 
 // UI Logic
 
-function printElements(apiResponse, city) {
-  document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%. 
-  The temperature in Kelvins is ${apiResponse.main.temp} degrees.`;
+function printElements(response, city) {
+  document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${response.main.humidity}%.
+  The temperature in Kelvins is ${response.main.temp} degrees.`;
 }
 
-function printError(request, apiResponse, city) {
-  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
+function printError(error, city) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: 
+  ${error}.`;
 }
 
 function handleFormSubmission(event) {
