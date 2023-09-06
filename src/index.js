68adfec59ad0c045f3cf62418351640fd06063a6
sethgonzales <sethgonzales157@gmail.com>
2023-09-06 10:35:57 -0700

    comment functions of new code

--- src/index.js
+++ src/index.js
@@ -1,40 +1,40 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import WeatherService from './weather-service.js';
+import WeatherService from './weather-service.js'; //import the WeatherService class
 
 // Business Logic
 
-function getWeather(city) {
-  WeatherService.getWeather(city)
-    .then(function(response) {
-      if (response.main) {
-        printElements(response, city);
-      } else {
-        printError(response, city);
+function getWeather(city) { //function to gather city information from the api. Is called after form is submitted with city data.
+  WeatherService.getWeather(city) //dot notation to find the city data from the api 
+    .then(function(response) { //promise chain... Waits for api response... then runs a function passing through the response
+      if (response.main) { //if the response data has a property called "main"...
+        printElements(response, city); //then we call on this method to show the weather
+      } else { //if it does not have this property...
+        printError(response, city); //we call on this method to show the error
       }
     });
 }
 
 // UI Logic
 
-function printElements(response, city) {
+function printElements(response, city) { //shows what we need to print on the screen when giving the weather data.
   document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${response.main.humidity}%.
-  The temperature in Kelvins is ${response.main.temp} degrees.`;
+  The temperature in Kelvins is ${response.main.temp} degrees.`; //can also use .innerHTML to create new html formatting from JS.
 }
 
-function printError(error, city) {
+function printError(error, city) { //shows what we need to print on the screen if an error happens
   document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: 
   ${error}.`;
 }
-
-function handleFormSubmission(event) {
+ 
+function handleFormSubmission(event) { //gathers data from the form event and resets the form entries
   event.preventDefault();
   const city = document.querySelector('#location').value;
   document.querySelector('#location').value = null;
-  getWeather(city);
+  getWeather(city); //calls on the getWeather function, passing in the city taken in from the form
 }
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function() { //event listeners
   document.querySelector('form').addEventListener("submit", handleFormSubmission);
 });
--- src/weather-service.js
+++ src/weather-service.js
@@ -1,16 +1,19 @@
-export default class WeatherService {  //create WeatherService class that acts as instance of API
-  static getWeather(city) { //
-    return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`)
-      .then(function(response) {
-        if (!response.ok) {
-          const errorMessage = `${response.status} ${response.statusText}`;
-          throw new Error(errorMessage);
+//This file holds our connection to the API
+
+export default class WeatherService {  //create WeatherService class to interact with API
+  static getWeather(city) { //call this method on the class itself without creating a new instance, just retrieve data.
+    return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`) //use fetch to find the url of the api and enter our key so we can make requests
+      .then(function(response) { //then... once fetch has successfully completed, the response that is given by the api status is checked
+        if (!response.ok) { //if NOT okay...
+          const errorMessage = `${response.status} ${response.statusText}`; //create a variable for the error status message
+          throw new Error(errorMessage); //throw a new Error including the error message, and stop what we are doing.
         } else {
-          return response.json();
+          return response.json(); //if everything is okay, response.json will parse the api object into JSON lang 
         }
-      })      
-      .catch(function(error) {
-        return error;
+      })      //end of the '.then' block
+
+      .catch(function(error) { //If there is an error read, this will "catch" the error that was "thrown" and will handle the error
+        return error; //return the error object
       });
   }
 }
